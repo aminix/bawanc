f865e9c4c85ffe6314fc48ce0c9b8047c8e9d1d8
c00lius <juliusempty@julius.local>
2015-08-29 23:03:34 +0200

    Testing Git
    I played around with unity to see which settings are best, we are going
    to redo everything tomorrow. This is just for testing.

---
+++ bawanc/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Rigidbody rb = GetComponent<Rigidbody> ();
+		float x = Input.GetAxis ("Horizontal") * 1000 * Time.deltaTime;
+		float y = Input.GetAxis ("Vertical") * 1000 * Time.deltaTime;
+		Debug.Log (x);
+		Vector3 f = new Vector3 (x, y, 0);
+		rb.AddForce (f);
+	}
+}

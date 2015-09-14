d713744cfb419f81c0c147662c5aae104f27c448
Mark Angelo Monteras <mark.monteras620@gmail.com>
2015-09-14 15:45:34 +0800

    Initial Main Menu in InitialScene.unity

---
+++ Assets/Scripts/UI Scripts/MainMenuScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+     public void QuitGame()
+    {
+        Application.Quit(); // Will work once build and tested.. i think
+    }
+
+    public void StartGame()
+    {
+        Application.LoadLevel("Level 1"); 
+    }
+}

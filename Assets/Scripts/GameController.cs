319ce4fd0381f33d1329d37771a3e22ff459f93a
aminix <amina.layco@gmail.com>
2015-09-16 00:53:30 -0300

    re arranged folder for unused scripts, added game scene folder with logic to move from scene to scene, game controller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public static GameController Instance;
+
+	public Vector3 respawnPoint; //point where character will respawn when he dies, other scripts will change it
+	private GameObject character;
+
+	void Awake()
+	{
+		Instance = this;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		respawnPoint = new Vector3 (1, 0, 0); //first point
+		character = GameObject.FindWithTag ("Player");
+		character.transform.position = respawnPoint;
+	
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	//Call when character dies, should be called from the object that killed the character
+	public void respawnCharacter() 
+	{
+		Debug.Log ("Character died :(");
+		character.transform.position = respawnPoint;
+	}
+
+	//Call when player clicks Start Game
+	public void GameStart() 
+	{
+		Debug.Log ("Start game");
+	}
+
+	//Call when player finishes the game or quit
+	public void GameOver() 
+	{
+		Debug.Log ("Player finished game");
+		respawnPoint = new Vector3 (1, 0, 0); //first point
+		character.transform.position = respawnPoint;
+	}
+
+
+}
---
+++ Assets/Scripts/NextLevelTrigger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * This class should go attached to an invisible object
+ * at the end of each level and triggers the next one
+ * 
+ */
+public class NextLevelTrigger : MonoBehaviour {
+	public int nextSceneNumber;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		Debug.Log ("Next level is " + nextSceneNumber);
+		if (other.tag == "Player") {
+			Object.DontDestroyOnLoad (GameController.Instance);
+			Application.LoadLevel (nextSceneNumber);
+		}
+
+	}
+}

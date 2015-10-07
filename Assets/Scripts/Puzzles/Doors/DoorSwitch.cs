00dfbb4299d01ae48155ee0ae249d8ceba366287
ironboundmanzer <manikantlawania@gmail.com>
2015-10-07 14:34:13 +0530

    I have made a door
    have made a model, a script, animations.
    the door has options of Automatic opening , opening with Key (not
    keyboard key but lock key [ can add a finding dimension in the game ],
    if the door is hackable [ I just overdid it with this one i guess] and
    OF COURSE the door can be destroyed.
    Destroying the doors doesnt look beautiful though.

---
+++ Assets/Scripts/Puzzles/Doors/DoorSwitch.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorSwitch : MonoBehaviour {
+
+	//public static
+
+
+	//public
+	public bool AutomaticOpeningDoor;
+	public bool KeyNeededToOpenDoor;
+	public bool IsItHackable;
+	public bool GateOpen;
+
+	//private
+	private Animator anim;
+	private float timer;
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if(GateOpen)
+		{
+			timer += Time.deltaTime;
+		}
+		if(timer > 2)
+		{
+			GateOpen = false;
+			timer = 0;
+		}
+	}
+
+	void OnCollisionEnter(Collision coll)
+	{
+
+		if(coll.gameObject.tag == "Player" && !GateOpen)
+		{
+			if(KeyNeededToOpenDoor)
+			{
+				Debug.Log("Please bring desired key");
+			}
+			else
+			{
+				if(AutomaticOpeningDoor)
+				{
+					Vector3 df = (transform.position - coll.gameObject.transform.position);
+					df = (new Vector3(df.x,0,df.z).normalized);
+
+					if(df.x > 0)
+					{
+						anim.SetTrigger("Open Sesame Right");
+					}
+					else
+					{
+						anim.SetTrigger("Open Sesame Left");
+					}
+
+					GateOpen = true;
+				}
+
+			}
+
+		}
+
+
+
+		Debug.Log(coll.gameObject +" Collided");
+	}
+
+	void OnTriggerEnter(Collider coll)
+	{
+
+		Debug.Log(coll.gameObject+ " Triggered");
+	}
+
+
+}
